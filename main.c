70f1adb3a7f0ef93bfa87d15f5bc86b5066e508f
Prashanth Ramachandran <prashanth.ramachandran@chargepoint.com>
2022-04-11 14:10:02 -0700

    Fixed the print statement

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int main() {
   printf("Hello %d\n", 1+3);
   printf("Mult %d\n", 3 * 2);
   printf("Division %2.1f\n", 3.0/2.0);
-  printf("Division with 0 in the denominator\n", 4/0);
+  printf("Division with 0 in the denominator %d\n", 4/0);
   return 0;
 }
