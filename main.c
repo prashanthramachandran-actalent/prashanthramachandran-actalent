09e9521f66265ade196f23db8d28fc4702620df4
Prashanth Ramachandran <prashanth.ramachandran@chargepoint.com>
2022-04-12 15:15:07 -0700

    Added exponent

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
   printf("Hello World\n");
   printf("Hello %d\n", 1+3);
   printf("Mult %d\n", 3 * 2);
   printf("Division %2.1f\n", 3.0/2.0);
+  printf("Exponent of 3 to base 2 is %.2lf\n",pow(2,3));
+
   return 0;
 }
