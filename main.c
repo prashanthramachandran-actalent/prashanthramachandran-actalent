e882666ca02356d7e20a6a12e13de9ea0669a8e5
Prashanth Ramachandran <prashanth.ramachandran@chargepoint.com>
2022-04-14 11:07:26 -0700

    Added logarithm

--- main.c
+++ main.c
@@ -7,6 +7,6 @@ int main() {
   printf("Mult %d\n", 3 * 2);
   printf("Division %2.1f\n", 3.0/2.0);
   printf("Exponent of 3 to base 2 is %.2lf\n",pow(2,3));
-
+  printf("Logarithm of 4  is %2.1f\n",log(4.0));
   return 0;
 }
