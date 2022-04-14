60ee64848e9a3dbf3c9e321105ec6d4323053d41
Prashanth Ramachandran <prashanth.ramachandran@chargepoint.com>
2022-04-14 13:56:42 -0700

    Subraction of the power and log\n

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main() {
   printf("Mult %d\n", 3 * 2);
   printf("Division %2.1f\n", 3.0/2.0);
   printf("Exponent of 3 to base 2 is %.2lf\n",pow(2,3));
+  printf("Subraction of the results %f\n",pow(2,3) - log(4.0));
   printf("Logarithm of 4  is %2.1f\n",log(4.0));
   printf("Sine wave with value 0.5 is %f\n",sin(0.5));
   printf("Cosine wave with value 0.5 is %f\n",cos(0.5));
