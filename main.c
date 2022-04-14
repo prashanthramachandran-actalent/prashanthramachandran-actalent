7dd231a5010342395b39569d38a9df900cf208be
Prashanth Ramachandran <prashanth.ramachandran@chargepoint.com>
2022-04-14 13:13:33 -0700

    added subraction

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main() {
   printf("Hello World\n");
   printf("Hello %d\n", 1+3);
+  printf("Subraction %d\n", 2-3);
   printf("Mult %d\n", 3 * 2);
   printf("Division %2.1f\n", 3.0/2.0);
   printf("Exponent of 3 to base 2 is %.2lf\n",pow(2,3));
